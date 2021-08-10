37b980924ecd42552ecdc1cced18ff4d7933931c
Kwang <kwangene@hotmail.com>
2021-08-10 11:28:27 +0200

    Movie Selector

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.Occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+
+let  ticketPrice = +movieSelect.value;
+
+//update total and count
+function updateSelectedCount(){
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+ const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+
+}
+// movie select event
+
+movieSelect.addEventListener('change',e =>{
+   ticketPrice = +e.target.value;
+   updateSelectedCount();  
+});
+
+container.addEventListener('click',function(e){
+ if((e.target.classList.contains('seat')) && !( e.target.classList.contains('Occupied')))
+ {
+  e.target.classList.toggle('selected');
+  updateSelectedCount();
+ }
+
+});
